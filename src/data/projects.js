b4e888fd460d6004d4027e1a0ee5d3d3bcb1300a
Beier Luo <beierluo001@gmail.com>
2025-10-29 23:15:25 +0800

    Add Blog

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ const projects = {
       title: "Your Pre-trained LLM is Secretly an Unsupervised Confidence Calibrator",
       taglineMarkdown: "Beier Luo, Shuoyuan Wang, Sharon Li, Hongxin Wei",
       descriptionMarkdown: "",
-      linkMarkdown: "[Paper](https://openreview.net/pdf?id=I4PJYZvfW5) [Code](https://github.com/ml-stat-Sustech/Disagreement-Aware-Calibration)",
+      linkMarkdown: "[Paper](https://openreview.net/pdf?id=I4PJYZvfW5) [Code](https://github.com/ml-stat-Sustech/Disagreement-Aware-Calibration) [Blog](./blog/your_pre/your_pre.html)",
       badge: "NeurIPS · 2025",
       accent: "default"
     }
