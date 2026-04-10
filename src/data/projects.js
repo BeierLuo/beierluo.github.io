006c8006353e2ca1293118e4cbbdb555212d203a
Beier <beier@BeierdeMac-mini.local>
2026-04-10 15:03:44 +0800

    Update paper link to OpenReview forum page

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ const projects = {
       taglineMarkdown: "Beier Luo, Shuoyuan Wang, Sharon Li, Hongxin Wei",
       descriptionMarkdown: "",
       links: [
-        { label: "Paper", href: "https://openreview.net/pdf?id=I4PJYZvfW5" },
+        { label: "Paper", href: "https://openreview.net/forum?id=I4PJYZvfW5" },
         { label: "Code", href: "https://github.com/ml-stat-Sustech/Disagreement-Aware-Calibration" },
         { label: "Blog", href: "./blog/your_pre/your_pre.html" }
       ],
